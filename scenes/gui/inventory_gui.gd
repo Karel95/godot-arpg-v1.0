22bf387967bec2b6c0f5a5b44a1971deb27a0db6
Karel <karelh2207@gmail.com>
2024-10-10 19:23:01 -0300

    refactoring

--- scenes/gui/inventory_gui.gd
+++ scenes/gui/inventory_gui.gd
@@ -3,16 +3,29 @@ extends Control
 var isOpen:bool = false
 
 @onready var inventory: Inventory = preload("res://inventory/playerInventory.tres")
+@onready var itemsPanelClass = preload("res://scenes/gui/items_panel.tscn")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
+var itemInHand: ItemsPanel
+
 func _ready():
+	connectSlots()
 	inventory.updated.connect(update)
 	update()
 	visible = isOpen
 
 func update():
 	for i in range(min(inventory.slots.size(), slots.size())):
-		slots[i].update(inventory.slots[i])
+		var inventorySlot: InventorySlot = inventory.slots[i]
+		if !inventorySlot.item: continue
+		
+		var itemsPanel: ItemsPanel = slots[i].itemsPanel
+		if !itemsPanel:
+			itemsPanel = itemsPanelClass.instantiate()
+			slots[i].insert(itemsPanel)
+			
+		itemsPanel.inventorySlot = inventorySlot
+		#itemsPanel.update()
 
 func open():
 	visible = true
@@ -23,3 +36,42 @@ func close():
 	visible = false
 	isOpen = false
 	
+
+func connectSlots():
+	for i in range(slots.size()):
+		var slot = slots[i]
+		slot.index = i
+		
+		var callable = Callable(onSlotClicked)
+		callable = callable.bind(slot)
+		slot.pressed.connect(callable)
+
+func onSlotClicked(slot):
+	if slot.isEmpty() && itemInHand:
+		insertItemInSlot(slot)
+		return
+	
+	if !itemInHand:
+		takeItemFromSlot(slot)
+
+func takeItemFromSlot(slot):
+	itemInHand = slot.takeItem()
+	add_child(itemInHand)
+	updateItemInHand()
+
+func insertItemInSlot(slot):
+	var item = itemInHand
+	
+	remove_child(itemInHand)
+	itemInHand = null
+	
+	slot.insert(item)
+
+func updateItemInHand():
+	if !itemInHand: return
+	itemInHand.global_position = get_global_mouse_position() - itemInHand.size/2
+
+func _input(event):
+	updateItemInHand()
+
+
---
+++ scenes/gui/slot_button.gd
@@ -0,0 +1,34 @@
+extends Button
+
+@onready var backgroundSprite: Sprite2D = $background
+@onready var container: CenterContainer = $CenterContainer
+
+@onready var inventory = preload("res://inventory/playerInventory.tres")
+
+var itemsPanel: ItemsPanel
+var index: int
+
+func insert(items: ItemsPanel):
+	itemsPanel = items
+	backgroundSprite.frame = 1
+	container.add_child(itemsPanel)
+	
+	if !itemsPanel.inventorySlot || inventory.slots[index] == itemsPanel.inventorySlot:
+		return
+	
+	inventory.insertSlot(index, itemsPanel.inventorySlot)
+
+func takeItem():
+	var item = itemsPanel
+	
+	container.remove_child(itemsPanel)
+	itemsPanel = null
+	backgroundSprite.frame = 0
+	
+	return item
+
+func isEmpty():
+	return !itemsPanel
+
+
+
